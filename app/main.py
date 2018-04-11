468fd928ddff37be5b44733cdafdc6b78a720bce
Daniel Chen <dchen7321@gmail.com>
2018-04-11 09:26:24 -0400

    json files added.

--- app/main.py
+++ app/main.py
@@ -25,83 +25,82 @@ def index():
 #===================================================================================
 @app.route('/iron_man')
 def iron_man():
-	return render_template('iron_man.html', jsondata=charInMovie(1))
+	return render_template('iron_man.html')
 
 @app.route('/the_incredible_hulk')
 def the_incredible_hulk():
-	return render_template('the_incredible_hulk.html', jsondata=charInMovie(2))
+	return render_template('the_incredible_hulk.html')
 
 @app.route('/iron_man_2')
 def iron_man_2():
-	return render_template('iron_man_2.html', jsondata=charInMovie(3))
+	return render_template('iron_man_2.html')
 
 @app.route('/thor')
 def thor():
-	return render_template('thor.html', jsondata=charInMovie(4))
+	return render_template('thor.html')
 
 @app.route('/captain_america_the_first_avenger')
 def captain_america_the_first_avenger():
-	return render_template('captain_america_the_first_avenger.html', jsondata=charInMovie(5))
+	return render_template('captain_america_the_first_avenger.html')
 
 @app.route('/the_avengers')
 def the_avengers():
-	return render_template('the_avengers.html', jsondata=charInMovie(6))
+	return render_template('the_avengers.html')
 
 @app.route('/iron_man_3')
 def iron_man_3():
-	return render_template('iron_man_3.html', jsondata=charInMovie(7))
+	return render_template('iron_man_3.html')
 
 @app.route('/thor_dark_world')
 def thor_dark_world():
-	return render_template('thor_dark_world.html', jsondata=charInMovie(8))
+	return render_template('thor_dark_world.html')
 
 @app.route('/captain_america_the_winter_soldier')
 def captain_america_the_winter_soldier():
-	return render_template('captain_america_the_winter_soldier.html', jsondata=charInMovie(9))
+	return render_template('captain_america_the_winter_soldier.html')
 
 @app.route('/guardians_of_galaxy')
 def guardians_of_the_galaxy():
-	jsondata=charInMovie(10)
-	return render_template('guardians_of_galaxy.html', jsondata=jsondata)
+	return render_template('guardians_of_galaxy.html')
 
 @app.route('/avengers_age_of_ultron')
 def avengers_age_of_ultron():
-	return render_template('avengers_age_of_ultron.html', jsondata=charInMovie(11))
+	return render_template('avengers_age_of_ultron.html')
 
 @app.route('/ant_man')
 def ant_man():
-	return render_template('ant_man.html', jsondata=charInMovie(12))
+	return render_template('ant_man.html')
 
 @app.route('/captain_america_civil_war')
 def captain_america_civil_war():
-	return render_template('captain_america_civil_war.html', jsondata=charInMovie(13))
+	return render_template('captain_america_civil_war.html')
 
 @app.route('/doctor_strange')
 def doctor_strange():
-	return render_template('doctor_strange.html', jsondata=charInMovie(14))
+	return render_template('doctor_strange.html')
 
 @app.route('/guardians_of_the_galaxy_vol_2')
 def guardians_of_the_galaxy_vol_2():
-	return render_template('guardians_of_the_galaxy_vol_2.html', jsondata=charInMovie(15))
+	return render_template('guardians_of_the_galaxy_vol_2.html')
 
 @app.route('/spider_man_homecoming')
 def spider_man_homecoming():
-	return render_template('spider_man_homecoming.html', jsondata=charInMovie(16))
+	return render_template('spider_man_homecoming.html')
 
 @app.route('/thor_ragnarok')
 def thor_ragnarok():
-	return render_template('thor_ragnarok.html', jsondata=charInMovie(17))
+	return render_template('thor_ragnarok.html')
 
 @app.route('/black_panther')
 def black_panther():
-	return render_template('black_panther.html', jsondata=charInMovie(18))
+	return render_template('black_panther.html')
 #=================================================================================
 
 
 
-@app.route('/test')
-def testing():
-	return charInMovie(15)
+@app.route('/test/<movie_id>')
+def testing(movie_id):
+	return charInMovie(movie_id)
 
 def charInMovie(movie_ID):
 	cur = cnn.cursor()
@@ -138,8 +137,8 @@ def charInMovie(movie_ID):
 		eventNodes = result.fetchall()
 	print(movieNode)
 
-	result_json = make_response(jsonify({'nodes': jsonifyNodes(charNodes, orgNodes, movieNode, eventNodes)
-		,"links" : createLinks(links)}), 201)
+	result_json = jsonify({'nodes': jsonifyNodes(charNodes, orgNodes, movieNode, eventNodes)
+		,"links" : createLinks(links)})
 	return result_json
 
 def createLinks(links):
