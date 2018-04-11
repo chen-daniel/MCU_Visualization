8ba732b15766462c21434fc7de2ddeb8a55cfa98
Tam Vu <vu.t@husky.neu.edu>
2018-04-10 22:45:03 -0400

    full jsonify

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app import app
 from app.user import views
 from flask import render_template, flash, redirect, url_for, request, current_app, session
-from flask_mysqldb import MySQL
+from flask.ext.mysqldb import MySQL
 
 import json
 
@@ -30,13 +30,27 @@ def charInMovie(movie):
   cur = mysql.connection.cursor()
   cur.execute('''CALL char_in_movie(1)''')
   rv = cur.fetchall()
-  return jsonifyChars(rv)
+  return {
+   "nodes" : jsonifyChars(char),
+   "links" : linkChar(char)}
 
-def jsonifyChars(char):
-   payload = []
-   content = {}
-   for result in rv:
-       content = {
+def linkChar(char):
+	payload = []
+   i = 0
+   for c1 in char:
+   	if i >= enumerate(char): break
+   	for c2 in char:
+   		if i >= enumerate(char): break
+   		elif(c1[0] != c2[0]):  
+   			payload.append(makeLink(c1, c2))
+   			i = i + 1
+   return jsonify(payload)
+
+def makeLink(char1, char2):
+	return {"source": char1[1], "target": char2[1], "value": 100}
+
+def jsonifyAChar(c): 
+	return {
        'id': result[0], 
        'alias': result[1], 
        'name': result[2],
@@ -44,6 +58,11 @@ def jsonifyChars(char):
        'species': result[4],
        'first_movie': result[5]}
 
+def jsonifyChars(char):
+   payload = []
+   content = {}
+   for result in char:
+       content = jsonifyAChar(result)
        payload.append(content)
        content = {}
    return jsonify(payload)
