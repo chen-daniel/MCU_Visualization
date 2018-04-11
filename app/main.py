68b569800f74262c12844ea7563c0e156e70f2e6
Tam Vu <vu.t@husky.neu.edu>
2018-04-11 04:04:50 -0400

    finish JSON

--- app/main.py
+++ app/main.py
@@ -32,58 +32,59 @@ def guardians_of_galaxy():
 	return render_template('guardians_of_galaxy.html')
 
 # Tam's stuff
-query = "CALL char_in_movie(1)"
-@app.route('/temp')
-def temp():
-	cur = cnn.cursor()
-	cur.execute(query, multi = True)
-	rv = cur.fetchall()
-	return(jsonify(rv))
-
+query1 = "CALL char_in_movie(1)"
+query2 = "SELECT * FROM characters C JOIN movies_characters MC ON 1 =  MC.movie_id WHERE C.character_id = MC.character_id"
+query = "SELECT * FROM characters"
+@app.route('/temp/<movie>')
+# def temp():
+# 	print("hi")
+# 	cur = cnn.cursor()
+# 	cur.execute(query1)
+# 	rv = cur.fetchall()
+# 	return(jsonify(rv))
 
 def charInMovie(movie):
-	
 	#cur = mysql.connection.cursor()
 	cur = cnn.cursor()
-	cur.execute(query, multi = True)
-	print("hi")
+	cur.execute(query2)
 	rv = cur.fetchall()
-	print {
-	"nodes" : jsonifyChars(rv),
-	"links" : linkChar(rv)}
+	print(rv)
+	#return jsonifyChars(rv)
+	return jsonify({'nodes': jsonifyChars(rv)
+		,"links" : linkChar(rv)})
 
 def linkChar(char):
 	payload = []
 	i = 0
 	for c1 in char:
-		if i >= enumerate(char) - 1 : 
-			break
+		# if i >= enumerate(char): 
+		# 	break
 		for c2 in char:
-			if i >= enumerate(char) - 1: 
-				break 
-			elif c1[0] != c2[0]:
+			# if i >= enumerate(char) - 1: 
+			# 	break 
+			if c1[0] != c2[0]:
 				payload.append(makeLink(c1, c2))
 				i = i + 1
-	return jsonify(payload)
+	return payload
 
 def makeLink(char1, char2):
 	return {"source": char1[1], "target": char2[1], "value": 200}
 
 def jsonifyChars(char):
-	print("hi")
-   payload = []
-   content = {}
-   for result in char:
-	   content = jsonifyAChar(result)
-	   payload.append(content)
-	   content = {}
-   return jsonify(payload)
+	payload = []
+	content = {}
+	for result in char:
+		content = jsonifyAChar(result)
+		payload.append(content)
+		content = {}
+	return payload
+		#return jsonify(payload)
 
-def jsonifyAChar(result): 
+def jsonifyAChar(result):
 	return {
-	'id': result[0],
+	'id': result[1],
 	'group': result[6],
-	'image':result[5],
+	'image': result[5],
 	'about': jsonAbout(result),
 	'events': eventToString(result)}
 
@@ -91,16 +92,20 @@ def jsonAbout(char):
 	return [char[2], gender(char[3]), char[4]]
 
 def gender(code):
-	if code == "F": return "Female"
-	else return "Male"
+	if code == 'F': return 'Female'
+	else: return 'Male'
+
 
 def eventToString(char):
 	cur = cnn.cursor()
-	cur.execute("CALL eventDes(%s)", (char), multi = True)
-	rv = cur.fetchone()
+	char_id = char[0]
+	#query3 = ("CALL eventDes('%s')") % char_id
+	query_event = ("SELECT description FROM events_characters WHERE character_id = '%s'") % char_id
+	cur.execute(query_event)
+	rv = cur.fetchall()
 	ans = ""
 	for des in rv:
-		ans = ans + des;
+		ans = ans + des[0];
 	return ans;
 
 
